a97ecfd8f0ad0979d45ee4598deb590262f64410
Tim Horist <TimelikeClosure@users.noreply.github.com>
2020-01-28 18:19:46 -0800

    add user command list notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // Available Operations:
+    // - "List departments" - list departments alphabetically
+    // - "List employees" - list employees alphabetically
+    // - "List employees by department" - list employees and their dept, grouped by dept. alphabetically, sorted alphabetically
+    // - "List employees in {department}" - list employees in a dept, sorted alphabetically
+    // - "Commission {department}" - create new department
+    // - "Add {employee} to {department}" - create new employee under department
+    // - "Move {employee} from {department} to {department}" - move employee from first department to second
+    // - "Pull {employee} from {department}" - remove employee from department
+    // - "Dissolve {department}" - remove department and all employees in it
 }
