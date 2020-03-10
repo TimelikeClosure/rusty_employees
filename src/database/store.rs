948af94ac0df4255379243df068bc511cc6e7992
Tim Horist <TimelikeClosure@users.noreply.github.com>
2020-03-10 07:46:05 -0700

    add store tests

--- src/database/store.rs
+++ src/database/store.rs
@@ -37,3 +37,100 @@ impl Store {
         self.index.department_mut(department_name)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod store {
+        use super::*;
+
+        mod seed {
+            use super::Store;
+
+            #[test]
+            fn populates_store() {
+                let mut store = Store::new();
+
+                store.seed();
+
+                assert_ne!(0, store.departments().list().len());
+            }
+        }
+
+        mod departments {
+            use super::{Departments, Store};
+
+            #[test]
+            fn returns_departments() {
+                let store = Store::new();
+
+                assert_eq!(&(Departments::new()), store.departments());
+            }
+        }
+
+        mod departments_mut {
+            use super::{Departments, Store};
+
+            #[test]
+            fn returns_departments() {
+                let mut store = Store::new();
+
+                assert_eq!(&mut (Departments::new()), store.departments_mut());
+            }
+        }
+
+        mod department {
+            use super::{Department, QueryError, Store};
+
+            #[test]
+            fn department_exists() {
+                let mut store = Store::new();
+
+                store.departments_mut().create("Assets").unwrap();
+
+                assert_eq!(Ok(&(Department::new("Assets"))), store.department("Assets"));
+            }
+
+            #[test]
+            fn department_doesnt_exist() {
+                let store = Store::new();
+
+                assert_eq!(
+                    Err(QueryError::NotFound(
+                        "Department \"Twinkies\" not found".to_string()
+                    )),
+                    store.department("Twinkies")
+                );
+            }
+        }
+
+        mod department_mut {
+            use super::{Department, QueryError, Store};
+
+            #[test]
+            fn department_exists() {
+                let mut store = Store::new();
+
+                store.departments_mut().create("Liabilities").unwrap();
+
+                assert_eq!(
+                    Ok(&mut (Department::new("Liabilities"))),
+                    store.department_mut("Liabilities")
+                );
+            }
+
+            #[test]
+            fn department_doesnt_exist() {
+                let mut store = Store::new();
+
+                assert_eq!(
+                    Err(QueryError::NotFound(
+                        "Department \"Donuts\" not found".to_string()
+                    )),
+                    store.department_mut("Donuts")
+                );
+            }
+        }
+    }
+}
